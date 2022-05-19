e1b950865dba83e0b82a1a12b02d807064fa5b11
NickName73 <egorooz73@gmail.com>
2022-05-19 14:49:16 +0300

    Linux also doesnt worked

--- NiTiS.VE/Services/Runtime/MM/NMem.cs
+++ NiTiS.VE/Services/Runtime/MM/NMem.cs
@@ -6,9 +6,37 @@ using System.Text;
 namespace NiTiS.VE.Services.Runtime.MM;
 public static unsafe class NMem
 {
-	public const string NiTiSMemoryLibName = "nmeml.dll";
-	[DllImport(NiTiSMemoryLibName, CallingConvention = CallingConvention.Cdecl)]
-	public static extern byte* _alloc32B();
-	[DllImport(NiTiSMemoryLibName, CallingConvention = CallingConvention.Cdecl)]
-	public static extern byte* _unlock32B(byte* ptr);
+	private class Win
+	{
+		public const string NiTiSMemoryLibName = "nmeml.dll";
+		[DllImport(NiTiSMemoryLibName, CallingConvention = CallingConvention.Cdecl)]
+		public static extern byte* _alloc32B();
+		[DllImport(NiTiSMemoryLibName, CallingConvention = CallingConvention.Cdecl)]
+		public static extern void _unlock32B(byte* ptr);
+	}
+	private class Linux
+	{
+		public const string NiTiSMemoryLibName = "nmeml.lib";
+		[DllImport(NiTiSMemoryLibName, CallingConvention = CallingConvention.Cdecl)]
+		public static extern byte* _alloc32B();
+		[DllImport(NiTiSMemoryLibName, CallingConvention = CallingConvention.Cdecl)]
+		public static extern void _unlock32B(byte* ptr);
+	}
+	public static byte* _alloc32B()
+	{
+		if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+		{
+			return Win._alloc32B();
+		}
+		return Linux._alloc32B();
+	}
+	public static void _unlock32B(byte* ptr)
+	{
+		if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+		{
+			Win._unlock32B(ptr);
+			return;
+		}
+		Linux._unlock32B(ptr);
+	}
 }
