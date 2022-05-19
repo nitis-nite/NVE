568ce20b878988d63064992fe90b5fd2df4f15ee
NickName73 <egorooz73@gmail.com>
2022-05-19 15:49:48 +0300

    Uf

--- nmeml/nmeml.cpp
+++ nmeml/nmeml.cpp
@@ -1,51 +1,51 @@
 ﻿#include "nmeml.h"
 
 extern "C" {
-	nmenl char* _alloc32B() {
+	EXPORT char* _alloc32B() {
 		char* c = new char[32];
 		return c;
 	};
-	nmenl void _unlock32B(char* ptr) {
+	EXPORT void _unlock32B(char* ptr) {
 		delete[32] ptr;
 	};
 
 
 
-	nmenl char* _alloc128B() {
+	EXPORT char* _alloc128B() {
 		char* c = new char[128];
 		return c;
 	};
-	nmenl void _unlock128B(char* ptr) {
+	EXPORT void _unlock128B(char* ptr) {
 		delete[128] ptr;
 	};
 
 
 
-	nmenl char* _alloc256B() {
+	EXPORT char* _alloc256B() {
 		char* c = new char[256];
 		return c;
 	};
-	nmenl void _unlock256B(char* ptr) {
+	EXPORT void _unlock256B(char* ptr) {
 		delete[256] ptr;
 	};
 
 
 
-	nmenl char* _alloc512B() {
+	EXPORT char* _alloc512B() {
 		char* c = new char[512];
 		return c;
 	};
-	nmenl void _unlock512B(char* ptr) {
+	EXPORT void _unlock512B(char* ptr) {
 		delete[512] ptr;
 	};
 
 
 
-	nmenl char* _alloc1KB() {
+	EXPORT char* _alloc1KB() {
 		char* c = new char[1024];
 		return c;
 	};
-	nmenl void _unlock1KB(char* ptr) {
+	EXPORT void _unlock1KB(char* ptr) {
 		delete[1024] ptr;
 	};
 };
