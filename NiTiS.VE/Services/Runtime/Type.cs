573530daaaa9cf2eb068060e707c132a23cf7cf2
NickName73 <egorooz73@gmail.com>
2022-06-01 13:45:30 +0300

    Nya

--- NiTiS.VE/Services/Runtime/Type.cs
+++ NiTiS.VE/Services/Runtime/Type.cs
@@ -15,6 +15,33 @@ public class Type : IReferencable<Type>
 	public Reference<Package> Package { get; }
 	public class Builder
 	{
+		public Reference<Type> Reference { get; } = new();
+		public Reference<Package> Package { get; }
+		private TypeName name;
+		private readonly Sequence<object> methods = new(16);
+		public Builder(Reference<Package> package, TypeName name)
+		{
+			Package = package;
 
+		}
+		public Builder Method(object method)
+		{
+			return this;
+		}
+	}
+}
+[StructLayout(LayoutKind.Sequential)]
+public readonly struct TypeName
+{
+	public readonly Mstr space, name;
+	public TypeName(string space, string name)
+	{
+		this.space = new(space);
+		this.name = new(name);
+	}
+	public TypeName(Mstr space, Mstr name)
+	{
+		this.space = space;
+		this.name = name;
 	}
 }
